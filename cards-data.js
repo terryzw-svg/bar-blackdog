8c4f8587ae2cec05b409d1df8b2f4c7ebf5a4ad1
terryzw <terryzw@vip.qq.com>
2026-07-18 04:44:20 +0800

    add customer pages: query card + intro page

---
+++ cards-data.js
@@ -0,0 +1,37 @@
+// 月卡数据 - 自动导出于 2026-07-17
+window.__CARD_DATA__ = {
+  "cards": [
+    {
+      "id": "MK-20260708",
+      "name": "烈烈",
+      "total": 4,
+      "used": 1,
+      "startDate": "",
+      "expireDate": "2026-08-16"
+    },
+    {
+      "id": "MK-20260707",
+      "name": "周超",
+      "total": 4,
+      "used": 1,
+      "startDate": "",
+      "expireDate": "2026-08-16"
+    },
+    {
+      "id": "MK-20260706",
+      "name": "萌萌",
+      "total": 4,
+      "used": 1,
+      "startDate": "",
+      "expireDate": "2026-08-16"
+    },
+    {
+      "id": "MK-20260705",
+      "name": "生生",
+      "total": 4,
+      "used": 1,
+      "startDate": "",
+      "expireDate": "2026-08-16"
+    }
+  ]
+};
